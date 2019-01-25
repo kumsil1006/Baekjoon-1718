050e2030bd0229349320241f5ba34bb92a1c6447
Soobeen Yoon <39304306+kumsil1006@users.noreply.github.com>
2019-01-26 00:27:40 +0900

    Add files via upload
    백준 1718번 - 암호

---
+++ 소스.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+	string code;
+	
+	getline(cin, str); // 빈칸이 있을 수 있으므로 getline으로 받는다.
+	cin >> code;
+	
+	int i = 0;
+
+	while (str[i] != '\0') {
+		
+		for (int j = 0; j < code.length();j++) {
+			int s = str[i];
+			int c = code[j];
+
+			if (str[i] == ' ') 
+				cout << " ";	// 빈칸이면 빈칸ㄴ		
+			
+			else {
+				int s2 = s - c + 96; // 암호화
+				if (s2 < 97)  
+					s2 = s2 + 26; // 소문자의 범위보다 작은 수가 나오면 z로 돌아간다.
+				char cc2 = static_cast<char>(s2);
+				cout << cc2;
+			}
+
+			if (j == code.length() - 1) j = -1; // 코드 길이를 넘어서면 처음으로 돌아간다
+			
+			i++;
+			if (i == str.length()) break; // string 길이를 넘어서면 끝난다.
+		}
+		
+	}
+	
+}
